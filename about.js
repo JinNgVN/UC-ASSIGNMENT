d9450799f9ace119a3bc87b066e1d0f6846a9fd1
Ho Dac Dang Nguyen <u7180983@anu.edu.au>
2023-10-15 22:12:17 +1100

    finished about page

--- about.js
+++ about.js
@@ -1,21 +1,24 @@
-
 document.addEventListener("DOMContentLoaded", function() {
-    const slides = document.querySelectorAll('.about_info > div');
-    
+    const slides = document.querySelectorAll('.slide_wrapper');
+
     let observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
             }
         });
     }, {
-        threshold: 0.5 
+        threshold: 0,  // Trigger even when a tiny portion of the slide becomes visible.
+        rootMargin: "-150px 0px -150px 0px"  // Adjust this value to decide how far outside the viewport the callback should be triggered.
     });
 
     slides.forEach(slide => {
         observer.observe(slide);
     });
 });
+
 
 
 const accordionHeaders = document.querySelectorAll(".accordion-header");
@@ -37,3 +40,14 @@ accordionHeaders.forEach(header => {
 
 
 
+function toggleIcon(element) {
+    const icon = element.querySelector('i');
+    
+    if (icon.classList.contains('fa-plus')) {
+        icon.classList.remove('fa-plus');
+        icon.classList.add('fa-minus');
+    } else {
+        icon.classList.remove('fa-minus');
+        icon.classList.add('fa-plus');
+    }
+}
--- product.js
+++ product.js
@@ -1,1 +1,38 @@
+let isScrollingHorizontally = false;
+let lastScrollTopProduct = 0;
+let isScrollingDown = true;
+let accumulatedDelta = 0;
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+    isScrollingHorizontally = entry.isIntersecting;
+  });
+}, {
+    threshold: 0.1,  // Adjust this value
+});
+
+observer.observe(document.querySelector(".navigation"));
+
+window.addEventListener('scroll', (e) => {
+  const currentScrollTop = window.scrollY;
+  isScrollingDown = currentScrollTop > lastScrollTopProduct;
+  lastScrollTopProduct = currentScrollTop;
+  
+  if (isScrollingHorizontally) {
+    console.log("Moving now")
+    e.preventDefault();  // Prevent natural scrolling
+    const wideDiv = document.querySelector(".navigation");
+    const delta = isScrollingDown ? -10 : 10;
+    accumulatedDelta += delta;
+    
+    // Bounds checking for left and right ends of wide div
+    const maxTranslateX = -500 * window.innerWidth;  // Assuming vw is equivalent to the window width
+    const newTranslateX = Math.min(0, Math.max(maxTranslateX, accumulatedDelta));
+    wideDiv.style.transform = `translateX(${newTranslateX}px)`;
+    
+    // Allow natural scrolling to resume if at the rightmost end
+    if (newTranslateX === maxTranslateX) {
+      isScrollingHorizontally = false;
+    }
+  }
+});
