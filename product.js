596a08250d514b9bd71975fda770c9e66c1e6398
Ho Dac Dang Nguyen <u7180983@anu.edu.au>
2023-11-02 16:05:33 +1100

    Fix navBar bug, highlight nav

--- product.js
+++ product.js
@@ -1,6 +1,9 @@
 const wideDiv = document.querySelector('.navigation');
 const stickyParent = document.querySelector(".parent_wide_div");
 const nextSection = document.querySelector('footer');
+const navProducts = document.querySelectorAll('.nav_product');
+let previousActiveNav = null;
+
 
 // Artificially increase the document height by the width of the wide-div
 document.body.style.height = `${document.body.offsetHeight + wideDiv.offsetWidth - wideDiv.offsetHeight*2}px`;
@@ -11,16 +14,17 @@ window.addEventListener('scroll', function(e) {
     let position = window.scrollY;
 
     if (position > wideDivHeight) {
-        let adjustedPosition = position - wideDivHeight;
+      let adjustedPosition = position - wideDivHeight;
 
-        if (adjustedPosition <= threshold) {
-            wideDiv.style.transform = `translateX(-${adjustedPosition}px)`;
-            if (stickyParent.style.position !== 'sticky') {
-                stickyParent.style.position = 'sticky';  
-            }
-        } else {
-          wideDiv.style.transform = `translateX(-${threshold}px)`;
-        }
+      if (adjustedPosition <= threshold) {
+        hightlightNav(position);
+          wideDiv.style.transform = `translateX(-${adjustedPosition}px)`;
+          if (stickyParent.style.position !== 'sticky') {
+              stickyParent.style.position = 'sticky';  
+          }
+      } else {
+        wideDiv.style.transform = `translateX(-${threshold}px)`;
+      }
     }
 });
 
@@ -33,14 +37,41 @@ function navigateTo(index) {
   const itemWidth = stickyParent.offsetWidth;
 
   // Adjust the scroll position to match the translation
-  const adjustedScrollPosition = (itemWidth * index + nav.offsetHeight + imageProduct.offsetHeight);
+  let adjustedScrollPosition = (itemWidth * index + nav.offsetHeight + imageProduct.offsetHeight);
+  adjustedScrollPosition = adjustedScrollPosition === 715 ? 745 : adjustedScrollPosition;
+  console.log(adjustedScrollPosition);
   window.scrollTo(adjustedScrollPosition, adjustedScrollPosition);
 }
 
 // Add the event listeners to all nav_product elements
-const navProducts = document.querySelectorAll('.nav_product');
+
 navProducts.forEach((navProduct, index) => {
   navProduct.addEventListener('click', function() {
-      navigateTo(index);
+    toggleClassNav(navProduct);
+    navigateTo(index);
   });
 });
+
+const toggleClassNav = (navProduct) => {
+  if (previousActiveNav) {
+    previousActiveNav.classList.remove("select");
+  }
+  previousActiveNav = navProduct;
+  navProduct.classList.add("select");
+}
+
+const hightlightNav = (adjustedPosition) => {
+  if (adjustedPosition >= 745 && adjustedPosition <1931) {
+    toggleClassNav(navProducts[0]);
+  } else if (adjustedPosition >= 1931 && adjustedPosition <3300) {
+    toggleClassNav(navProducts[1]);
+  } else if (adjustedPosition >= 3300 && adjustedPosition <4700) {
+    toggleClassNav(navProducts[2]);
+  } else if (adjustedPosition >= 4700 && adjustedPosition <6300) {
+    toggleClassNav(navProducts[3]);
+  } else if (adjustedPosition >= 6300 && adjustedPosition <7700) {
+    toggleClassNav(navProducts[4]);
+  } else {
+    toggleClassNav(navProducts[5]);
+  }
+}
