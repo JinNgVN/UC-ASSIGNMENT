64f38212c365c12e3b33b79844fe529ee17ef217
Rex <nguyenho020198@gmail.com>
2023-11-05 00:27:23 +1100

    fix highlight nav bar

--- product.js
+++ product.js
@@ -2,9 +2,13 @@ const wideDiv = document.querySelector('.navigation');
 const stickyParent = document.querySelector(".parent_wide_div");
 const nextSection = document.querySelector('footer');
 const navProducts = document.querySelectorAll('.nav_product');
+const imageProduct = document.querySelector(".image");
+const nav = document.querySelector("header");
 let previousActiveNav = null;
 
-
+//get margin top
+const style = window.getComputedStyle(imageProduct);
+const margin_top_and_bottom =  parseInt(style.getPropertyValue('margin-top'), 10) * 2;
 // Artificially increase the document height by the width of the wide-div
 document.body.style.height = `${document.body.offsetHeight + wideDiv.offsetWidth - wideDiv.offsetHeight*2}px`;
 
@@ -16,8 +20,8 @@ window.addEventListener('scroll', function(e) {
     if (position > wideDivHeight) {
       let adjustedPosition = position - wideDivHeight;
 
-      if (adjustedPosition <= threshold) {
-        hightlightNav(position);
+      if (adjustedPosition < threshold) {
+        hightlightNav(Math.ceil(position));
           wideDiv.style.transform = `translateX(-${adjustedPosition}px)`;
           if (stickyParent.style.position !== 'sticky') {
               stickyParent.style.position = 'sticky';  
@@ -29,17 +33,15 @@ window.addEventListener('scroll', function(e) {
 });
 
 
-const imageProduct = document.querySelector(".image");
-const nav = document.querySelector("header");
+
 function navigateTo(index) {
   // Assuming each item in .navigation has the same width, and that width is 100vw
   // Adjust accordingly if this is not the case
   const itemWidth = stickyParent.offsetWidth;
 
   // Adjust the scroll position to match the translation
-  let adjustedScrollPosition = (itemWidth * index + nav.offsetHeight + imageProduct.offsetHeight);
-  adjustedScrollPosition = adjustedScrollPosition === 715 ? 745 : adjustedScrollPosition;
-  console.log(adjustedScrollPosition);
+  const adjustedScrollPosition = (itemWidth * index + nav.offsetHeight + imageProduct.offsetHeight + margin_top_and_bottom);
+  console.log("navigate to: ", adjustedScrollPosition);
   window.scrollTo(adjustedScrollPosition, adjustedScrollPosition);
 }
 
@@ -61,18 +63,32 @@ const toggleClassNav = (navProduct) => {
 }
 
 const hightlightNav = (adjustedPosition) => {
-  if (adjustedPosition >= 745 && adjustedPosition <1931) {
+  const itemWidth = stickyParent.offsetWidth;
+  const adjustedScrollPosition = ( nav.offsetHeight + imageProduct.offsetHeight + margin_top_and_bottom);
+  
+  if (adjustedPosition >= adjustedScrollPosition && adjustedPosition < adjustedScrollPosition + itemWidth) {
+    console.log(adjustedPosition);
     toggleClassNav(navProducts[0]);
-  } else if (adjustedPosition >= 1931 && adjustedPosition <3300) {
+  } else if (adjustedPosition >= adjustedScrollPosition + itemWidth && adjustedPosition < adjustedScrollPosition + itemWidth *2) {
     toggleClassNav(navProducts[1]);
-  } else if (adjustedPosition >= 3300 && adjustedPosition <4700) {
+    console.log(adjustedPosition);
+
+  } else if (adjustedPosition >= adjustedScrollPosition + itemWidth *2 && adjustedPosition < adjustedScrollPosition + itemWidth*3) {
     toggleClassNav(navProducts[2]);
-  } else if (adjustedPosition >= 4700 && adjustedPosition <6300) {
+    console.log(adjustedPosition);
+
+  } else if (adjustedPosition >= adjustedScrollPosition + itemWidth *3 && adjustedPosition < adjustedScrollPosition + itemWidth *4) {
     toggleClassNav(navProducts[3]);
-  } else if (adjustedPosition >= 6300 && adjustedPosition <7700) {
+    console.log(adjustedPosition);
+
+  } else if (adjustedPosition >= adjustedScrollPosition + itemWidth *4 && adjustedPosition < adjustedScrollPosition + itemWidth * 5) {
     toggleClassNav(navProducts[4]);
+    console.log(adjustedPosition);
+
   } else {
     toggleClassNav(navProducts[5]);
+    console.log(adjustedPosition);
+
   }
 }
 
